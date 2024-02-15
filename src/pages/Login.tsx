75a23019b0072fb62237d26692c11493395ebac1
Abu Jayed <aj.webdevlopment@gmail.com>
2024-02-15 15:29:28 +0600

    privet route component created.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,31 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-  const {register,handleSubmit} = useForm()
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm();
 
-  const [login,{data,error,isLoading,isSuccess}] = useLoginMutation()
-  
+  const [login, { data, error, isLoading, isSuccess }] = useLoginMutation();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log(data);
     const userInfo = {
       id: data.userId,
-      password: data.password
-    }
-    login(userInfo)
-  }
-  console.log("success",isSuccess);
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+
+    const user = verifyToken(res.data.accessToken)
+    console.log(user);
+
+    dispatch(setUser({user,token: res.data.accessToken}));
+
+  };
+  console.log("success", isSuccess);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
