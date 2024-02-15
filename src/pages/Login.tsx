fe893d4bf3d89cc87826c8972c4f502909f1c0d3
Abu Jayed <aj.webdevlopment@gmail.com>
2024-02-15 10:05:56 +0600

    able to login successfully.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
+  const {register,handleSubmit} = useForm()
+
+  const [login,{data,error,isLoading,isSuccess}] = useLoginMutation()
+  
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const userInfo = {
+      id: data.userId,
+      password: data.password
+    }
+    login(userInfo)
+  }
+  console.log("success",isSuccess);
+
   return (
-    <div>
-      This is our login page.
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" {...register("userId")} id="id" />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" {...register("password")} id="password" />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
